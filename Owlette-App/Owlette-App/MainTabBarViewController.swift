1aa4fae61167e0e32de0e1ccaf3ae2cee58592c7
Juan-Ceballos <juanceballos@pursuit.org>
2023-09-13 23:59:21 -0400

    updated main view to uiTabBarController, added in place holder viewControllers, set their tab bar item property to it's class with arguments to display tab bar ui

--- Owlette-App/Owlette-App/MainTabBarViewController.swift
+++ Owlette-App/Owlette-App/MainTabBarViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class MainTabBarViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
+    
+    public lazy var viewController1: UIViewController = {
+        let vc = ViewController1()
+        vc.tabBarItem = UITabBarItem(title: "VC1", image: UIImage(systemName: "heart"), tag: 0)
+        return vc
+    }()
+    
+    public lazy var viewController2: UIViewController = {
+        let vc = ViewController2()
+        vc.tabBarItem = UITabBarItem(title: "VC2", image: UIImage(systemName: "pencil"), tag: 1)
+        return vc
+    }()
+    
+    public lazy var viewController3: UIViewController = {
+        let vc = ViewController3()
+        vc.tabBarItem = UITabBarItem(title: "VC3", image: UIImage(systemName: "gear"), tag: 2)
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        viewControllers = [viewController1, viewController2, viewController3]
     }
+    
+    
 
 }
 
