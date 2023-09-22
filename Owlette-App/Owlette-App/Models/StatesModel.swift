d40dcb48b5ae9377b33f2bab56d67ad993ae9cc0
Juan-Ceballos <juanceballos@pursuit.org>
2023-09-22 19:55:53 -0400

    added states model as dictionary property for searches

---
+++ Owlette-App/Owlette-App/Models/StatesModel.swift
@@ -0,0 +1,63 @@
+//
+//  StatesModel.swift
+//  Owlette-App
+//
+//  Created by Juan ceballos on 9/22/23.
+//
+
+import Foundation
+
+struct StatesModel {
+    let statesDict = [
+        "Alabama": "AL",
+        "Alaska": "AK",
+        "Arizona": "AZ",
+        "Arkansas": "AR",
+        "California": "CA",
+        "Colorado": "CO",
+        "Connecticut": "CT",
+        "Delaware": "DE",
+        "Florida": "FL",
+        "Georgia": "GA",
+        "Hawaii": "HI",
+        "Idaho": "ID",
+        "Illinois": "IL",
+        "Indiana": "IN",
+        "Iowa": "IA",
+        "Kansas": "KS",
+        "Kentucky": "KY",
+        "Louisiana": "LA",
+        "Maine": "ME",
+        "Maryland": "MD",
+        "Massachusetts": "MA",
+        "Michigan": "MI",
+        "Minnesota": "MN",
+        "Mississippi": "MS",
+        "Missouri": "MO",
+        "Montana": "MT",
+        "Nebraska": "NE",
+        "Nevada": "NV",
+        "North Carolina": "NC",
+        "North Dakota": "ND",
+        "New Hampshire": "NH",
+        "New Mexico": "NM",
+        "New Jersey": "NJ",
+        "New York": "NY",
+        "Ohio": "OH",
+        "Oklahoma": "OK",
+        "Oregon": "OR",
+        "Pennsylvania": "PA",
+        "Rhode Island": "RI",
+        "South Carolina": "SC",
+        "South Dakota": "SD",
+        "Tennessee": "TN",
+        "Texas": "TX",
+        "Utah": "UT",
+        "Vermont": "VT",
+        "Virginia": "VA",
+        "Washington": "WA",
+        "West Virginia": "WV",
+        "Wisconsin": "WI",
+        "Wyoming": "WY"
+    ]
+}
