cb57f46202986695c95dd0bc27e5966251794860
Juan-Ceballos <juanceballos@pursuit.org>
2023-09-24 02:39:56 -0400

    changed var names in tab bar, removed tab bar titles

--- Owlette-App/Owlette-App/Controllers/MainTabBarViewController.swift
+++ Owlette-App/Owlette-App/Controllers/MainTabBarViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class MainTabBarViewController: UITabBarController {
     
-    public lazy var viewController1: UIViewController = {
+    public lazy var membersViewController: UIViewController = {
         let vc = MembersViewController()
-        vc.tabBarItem = UITabBarItem(title: "VC1", image: UIImage(systemName: "heart"), tag: 0)
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), tag: 0)
         return vc
     }()
     
     public lazy var viewController2: UIViewController = {
         let vc = ViewController2()
-        vc.tabBarItem = UITabBarItem(title: "VC2", image: UIImage(systemName: "pencil"), tag: 1)
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "book"), tag: 1)
         return vc
     }()
     
     public lazy var viewController3: UIViewController = {
         let vc = ViewController3()
-        vc.tabBarItem = UITabBarItem(title: "VC3", image: UIImage(systemName: "gear"), tag: 2)
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "gear"), tag: 2)
         return vc
     }()
 
@@ -31,7 +31,7 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
-        viewControllers = [viewController1, viewController2, viewController3]
+        viewControllers = [membersViewController, viewController2, viewController3]
     }
     
     
