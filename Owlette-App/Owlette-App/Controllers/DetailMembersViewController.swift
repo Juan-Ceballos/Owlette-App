97ceec5193cfe3b5427bbb224de2ec84cf7be65a
Juan-Ceballos <juanceballos@pursuit.org>
2023-10-03 23:28:02 -0400

    dependency injection, initializer for object on detail vc

--- Owlette-App/Owlette-App/Controllers/DetailMembersViewController.swift
+++ Owlette-App/Owlette-App/Controllers/DetailMembersViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class DetailMembersViewController: UIViewController {
 
+    var member: CGMembersModel?
+    
+    init(member: CGMembersModel? = nil) {
+        self.member = member
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
