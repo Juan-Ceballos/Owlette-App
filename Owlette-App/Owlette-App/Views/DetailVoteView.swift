83458c7d89876db7c129a0722fb53c7a206c5139
Juan-Ceballos <juanceballos@pursuit.org>
2023-10-27 23:57:12 -0400

    added background view and close button for detailView

--- Owlette-App/Owlette-App/Views/DetailVoteView.swift
+++ Owlette-App/Owlette-App/Views/DetailVoteView.swift
@@ -11,8 +11,14 @@ class DetailVoteView: UIView {
     
     let backgroundView: UIView = {
        let view = UIView()
-        view.backgroundColor = AppColors.tertiaryColor
+        view.backgroundColor = AppColors.primaryColor
         return view
+    }()
+    
+    let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        return button
     }()
     
     override init(frame: CGRect) {
@@ -27,5 +33,17 @@ class DetailVoteView: UIView {
     
     private func commonInit() {
         pinVCBackground(of: backgroundView)
+        setupCloseButtonConstraints()
+    }
+    
+    private func setupCloseButtonConstraints() {
+        addSubview(closeButton)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: AppSizes.medPadding),
+            closeButton.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -AppSizes.medPadding),
+            closeButton.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.2),
+            closeButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.2)
+        ])
     }
 }
