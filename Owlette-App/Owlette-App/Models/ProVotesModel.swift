2f976836b01bfd7e61bc16412000739c0566e421
Juan-Ceballos <juanceballos@pursuit.org>
2023-11-15 20:55:47 -0500

    updated vote model that will be used for detailVoteVC

--- Owlette-App/Owlette-App/Models/ProVotesModel.swift
+++ Owlette-App/Owlette-App/Models/ProVotesModel.swift
@@ -12,15 +12,62 @@ struct ProVotesContainer: Codable {
 }
 
 struct ProVotesModel: Codable {
+    let totalVotes: String
     let votes: [ProVote]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalVotes = "total_votes"
+        case votes
+    }
 }
 
 struct ProVote: Codable {
+    let rollCall: String
     let bill: ProVoteBill
+    let description: String
+    let question : String
+    let result: String
+    let date: String
+    let time: String
+    let total: [ProTotal]
     let position: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rollCall = "roll_call"
+        case bill
+        case description
+        case question
+        case result
+        case date
+        case time
+        case total
+        case position
+    }
+    
 }
 
 struct ProVoteBill: Codable {
     let number: String
     let title: String?
+    let latestAction: String
+    
+    enum CodingKeys: String, CodingKey {
+        case number
+        case title
+        case latestAction = "latest_action"
+    }
+}
+
+struct ProTotal: Codable {
+    let yes: Int
+    let no: Int
+    let present: Int
+    let notVoting: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case yes
+        case no
+        case present
+        case notVoting = "not_voting"
+    }
 }
