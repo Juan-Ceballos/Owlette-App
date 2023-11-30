43c3fa2f4a90fd8d4fa5d4d2a7c3eedd54e3625a
Juan-Ceballos <juanceballos@pursuit.org>
2023-11-30 15:10:43 -0500

    fetch and added statements to cells

--- Owlette-App/Owlette-App/Controllers/StatementsViewController.swift
+++ Owlette-App/Owlette-App/Controllers/StatementsViewController.swift
@@ -11,6 +11,7 @@ class StatementsViewController: UIViewController {
     
     let statementsView = StatementsView()
     var statements = [ProStatement]()
+    let proAPI = ProPublicaAPI()
     
     override func loadView() {
         view = statementsView
@@ -20,10 +21,29 @@ class StatementsViewController: UIViewController {
         super.viewDidLoad()
         statementsView.collectionView.delegate = self
         statementsView.collectionView.dataSource = self
+        Task {
+            await setupUI()
+        }
     }
     
-    private func fetchStatements() {
-        
+    private func fetchStatements() async -> ProStatementsContainer? {
+        do {
+            return try await proAPI.fetchParseData(pathComponent: "statements/latest.json", responseType: ProStatementsContainer.self)
+            
+        } catch {
+            print("error")
+        }
+        return nil
+    }
+    
+    
+    private func setupUI() async {
+        Task {
+            statements = await fetchStatements()?.results ?? []
+            DispatchQueue.main.async {
+                self.statementsView.collectionView.reloadData()
+            }
+        }
     }
 
 }
@@ -34,14 +54,15 @@ extension StatementsViewController: UICollectionViewDelegateFlowLayout {
 
 extension StatementsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        statements.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StatementCell.reuseId, for: indexPath) as? StatementCell else {
             fatalError("error")
         }
-        
+        let currentStatement = statements[indexPath.row]
+        cell.statementTextView.text = currentStatement.title
         return cell
     }
     
